f4ea2f530beae18caa7d04e53419088c4f25657e
Gordon06060 <gordonz0913@gmail.com>
2024-11-09 20:45:22 -0500

    fix: added spacing between headers

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,52 +3,9 @@ import { Chrono } from "react-chrono";
 import "../Experience.css";
 
 export default function Experience() {
-  const items = [
-    {
-      title: "Coding Lesson Developer and Executive",
-      cardTitle: "Coding Lesson Developer and Executive",
-      cardSubtitle: "Vancouver, FL",
-      cardDetailedText:
-        "Creative Direction, User Experience, Visual Design, Project Management, Team Leading",
-      date: "2023",
-    },
-    {
-      title: "Art Director",
-      cardTitle: "Art Director",
-      cardSubtitle: "San Francisco, CA",
-      cardDetailedText:
-        "Creative Direction, User Experience, Visual Design, SEO, Online Marketing",
-      date: "2010 - 2011",
-    },
-    {
-      title: "Web Designer",
-      cardTitle: "Web Designer",
-      cardSubtitle: "Los Angeles, CA",
-      cardDetailedText: "User Experience, Visual Design",
-      date: "2008 - 2010",
-    },
-    {
-      title: "Web Designer",
-      cardTitle: "Web Designer",
-      cardSubtitle: "San Francisco, CA",
-      cardDetailedText: "User Experience, Visual Design",
-      date: "2006 - 2008",
-    },
-  ];
-
   return (
-    <div className="timeline" style={{ width: "65vw", height: "100vh" }}>
+    <div className="timeline">
       <h1>Experience</h1>
-      <div className="chrono">
-        <Chrono
-          items={items}
-          slideShow
-          slideItemDuration={1000}
-          slideShowType="reveal"
-          mode="VERTICAL" // Adjust mode as needed
-          disableToolbar={true}
-        />{" "}
-      </div>
     </div>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,20 +5,22 @@ export default function Nav() {
     <>
       <div className="big-nav">
         <div className="nav-container">
-          <div className="home.nav">
-            <a href="#">Home</a>
-          </div>
-          <div className="about-nav">
-            <a href="#about">About</a>
-          </div>
-          <div className="experience-nav">
-            <a href="#experience">Experience</a>
-          </div>
-          <div className="projects-nav">
-            <a href="#projects">Projects</a>
-          </div>
-          <div className="skills-nav">
-            <a href="#skills">Skills</a>
+          <div className="home-container">
+            <div className="home.nav">
+              <a href="#">Home</a>
+            </div>
+            <div className="about-nav">
+              <a href="#about">About</a>
+            </div>
+            <div className="experience-nav">
+              <a href="#experience">Experience</a>
+            </div>
+            <div className="projects-nav">
+              <a href="#projects">Projects</a>
+            </div>
+            <div className="skills-nav">
+              <a href="#skills">Skills</a>
+            </div>
           </div>
         </div>
       </div>
